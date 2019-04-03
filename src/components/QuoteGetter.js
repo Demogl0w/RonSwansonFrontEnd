4ded282e9b9a24e0ce793111e3b66ad2edbdc59d
Demorio Fluker <Demorio@live.com>
2019-04-02 18:04:37 -0700

    Avoiding undefined quote behavior.

--- src/components/QuoteGetter.js
+++ src/components/QuoteGetter.js
@@ -14,7 +14,8 @@ const mapDispatchToProps = dispatch => ({
 
   //Dispatch that hits our server quote and quote info
   getQuote: size => {
-    fetch(`http://ron-quote-generator-backend.herokuapp.com/Quote/${size}`)
+    const thisSize = size || "";
+    fetch(`http://ron-quote-generator-backend.herokuapp.com/Quote/${thisSize}`)
       .then(res => res.json())
       .then(tempQuote => {
         console.log(tempQuote);
